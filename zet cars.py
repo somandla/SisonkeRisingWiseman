79f379442ee8acee0c6c6f1610c1287f58ce42e7
moralss <jeramoral@gmail.com>
2016-07-14 19:44:52 -0400

    made changes

---
+++ zet cars.py
@@ -0,0 +1,15 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def display_cars():
+	my_farvorite_cars={'bmw':1,'ford':2,'corola A5':3,'isuzu':4}
+	for iterm in my_farvorite_cars:
+		print(iterm)
+
+		
+>>> 
+>>> display_cars()
+ford
+corola A5
+bmw
+isuzu
+>>> 
